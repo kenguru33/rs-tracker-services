edff2865de20255eeff78fb6586f0cfe7cbe1671
Bernt Anker <bernt.anker@me.com>
2020-09-11 10:15:19 +0200

    changed envrinoment secrets variables

--- packages/aisdata-collector-kystverket/src/config.ts
+++ packages/aisdata-collector-kystverket/src/config.ts
@@ -11,7 +11,7 @@ export default () => ({
   kystverket: {
     url:
       'http://aisnorge.kystverket.no/ais/auth/requestServlet?REQUEST=ListTargets&layers=Aktive%20redningssk%C3%B8yter&INFO_FORMAT=application%2Fjson&CMR_FEATURE_ATTRIBUTES=MMSI,Destination,Decimal%20Latitude,Decimal%20Longitude,Ship%20name,Latitude,Longitude,SOG,COG,Time%20stamp',
-    login: process.env.KYSTVERKET_LOGIN,
+    login: process.env.KYSTVERKET_USERNAME,
     password: process.env.KYSTVERKET_PASSWORD,
     interval: parseInt(process.env.INTERVAL, 10) || 30000,
   },
