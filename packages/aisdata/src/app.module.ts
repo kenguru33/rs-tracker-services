de7c1fabfea10100f1ed06fb81a12b0b0782047b
Bernt Anker <bernt.anker@me.com>
2020-09-12 03:13:33 +0200

    Mongo DB setup

--- packages/aisdata/src/app.module.ts
+++ packages/aisdata/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://aisdata-mongo-srv'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
