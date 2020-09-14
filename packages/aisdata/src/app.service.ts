eb8246877d8412a96401225a5e1e8cfa6c0294a6
Bernt Anker <bernt.anker@me.com>
2020-09-14 12:22:12 +0200

    publish

--- packages/aisdata/src/app.service.ts
+++ packages/aisdata/src/app.service.ts
@@ -3,15 +3,17 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Aisdata } from './schemas/aisdata.schema';
 import { Model } from 'mongoose';
 import { CreateAisdataDto } from './dto/create-aisdata-dto';
+import { Publisher } from '@nestjs-plugins/nestjs-nats-streaming-transport';
+import { Patterns } from '@redningsselskapet/rs-tracker-services-common';
 
 @Injectable()
 export class AppService {
-  constructor(@InjectModel(Aisdata.name) private AisdataModel: Model<Aisdata> ) {}
+  constructor(@InjectModel(Aisdata.name) private AisdataModel: Model<Aisdata>, private publisher: Publisher ) {}
   async addAisdata(createAisdataDto: CreateAisdataDto):Promise<Aisdata> {
     const model = new this.AisdataModel(createAisdataDto)
     
     const result = await model.save()
-    console.log(result)
+    this.publisher.emit(Patterns.AisdataCreated, result)
     return result
   }
   getHello(): string {
